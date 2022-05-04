01bac60fe15a12109f5f6f01904e98fc1e8ef6a9
Anselm Schüler <mail@anselmschueler.com>
2022-05-04 23:22:11 +0200

    Formattier

--- Haskell/MandatoryTyping.hs
+++ Haskell/MandatoryTyping.hs
@@ -6,10 +6,13 @@ module Main (main) where
 
 import Data.Char (type Char)
 import Data.List ((++))
-import System.IO (type IO, getLine, putStr, putStrLn)
+import System.IO (getLine, putStr, putStrLn, type IO)
 
 main :: IO ()
-main = (do
-  (putStr :: [Char] -> IO ()) ("What is your name?  " :: [Char]) :: IO ()
-  name :: [Char] <- getLine :: IO [Char]
-  (putStrLn :: [Char] -> IO ()) ((((++) :: [Char] -> ([Char] -> [Char])) ("Hello, " :: [Char]) :: [Char] -> [Char]) (name :: [Char]) :: [Char]) :: IO ()) :: IO ()
+main =
+  ( do
+      (putStr :: [Char] -> IO ()) ("What is your name?  " :: [Char]) :: IO ()
+      name :: [Char] <- getLine :: IO [Char]
+      (putStrLn :: [Char] -> IO ()) ((((++) :: [Char] -> ([Char] -> [Char])) ("Hello, " :: [Char]) :: [Char] -> [Char]) (name :: [Char]) :: [Char]) :: IO ()
+  ) ::
+    IO ()
