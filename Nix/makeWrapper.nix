91e81f3b840f8e67ba57407bb15330b70cee3641
Anselm Schüler <mail@anselmschueler.com>
2022-05-23 21:41:26 +0200

    friqin

---
+++ Nix/makeWrapper.nix
@@ -0,0 +1,14 @@
+{ fish, makeWrapper }:
+{ source, outputs ? source.outputs, makeWrapperArgs }:
+
+with builtins;
+derivation {
+  name = source.name + "-wrapped";
+  inherit (source) system;
+  inherit outputs;
+
+  builder = fish;
+  args = [ (builtins.toFile script) ];
+/* FUCKING HELL */
+  inherit makeWrapperArgs;
+}
