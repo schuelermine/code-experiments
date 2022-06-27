97ce296ef60dfe58e138c96dd8c10143f45e61f2
Anselm Schüler <mail@anselmschueler.com>
2022-06-27 02:01:43 +0200

    Add problem folder

---
+++ Problems/lc1423-2.ts
@@ -0,0 +1,10 @@
+function maxScore(cardPoints: number[], k: number): number {
+    const add = (n: number, m: number) => n + m
+    let max = 0
+    for (var i = 0; i <= k; i++) {
+        const j = k - i
+        const score = cardPoints.slice(0, i).reduce(add, 0) + cardPoints.slice(-j).reduce(add, 0)
+        max = Math.max(max, score)
+    }
+    return max
+}
