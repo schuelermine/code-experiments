163f3fd5bcd00fb86fd4acd49be952b8f6bb9d00
Anselm Schüler <mail@anselmschueler.com>
2022-10-26 22:33:19 +0200

    strcmp

---
+++ C/strcmp.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int strcmp(const char *str1, const char *str2) {
+    int diff;
+    for (size_t i = 0; true; i++) {
+        diff = str1[i] - str2[i];
+        if (diff != 0) return diff;
+    }
+}
+
+int main(int argc, char** argv, char** envp) {
+    if (argc != 3) {
+        fprintf(stderr, "Invalid number of arguments!\n");
+        exit(22);
+    }
+
+    printf("%d", strcmp(argv[1], argv[2]));
+}
