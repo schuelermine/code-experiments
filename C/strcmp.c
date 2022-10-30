8a7de9bacc3decf49a7ed4086887f0eda7524fcd
Anselm Schüler <mail@anselmschueler.com>
2022-10-30 11:38:27 +0100

    fix C/strcmp.c

--- C/strcmp.c
+++ C/strcmp.c
@@ -5,6 +5,8 @@
 int strcmp(const char *str1, const char *str2) {
     int diff;
     for (size_t i = 0; true; i++) {
+        if (str1[i] == '\0' && str2[i] == '\0')
+            return 0;
         diff = str1[i] - str2[i];
         if (diff != 0) return diff;
     }
