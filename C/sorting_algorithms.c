d6500a47b670f5decc8c9a71ab72fe3c942360c3
Anselm Schüler <mail@anselmschueler.com>
2022-11-11 16:27:09 +0100

    fixed sorts

--- C/sorting_algorithms.c
+++ C/sorting_algorithms.c
@@ -1,6 +1,5 @@
 #include <stdbool.h>
 #include <stddef.h>
-#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,7 +8,6 @@
 
 void bubble_sort_in_place(int* array, size_t len) {
     if (len == 0) return;
-    assert(len > 0);
     for (size_t j = len - 1; j >= 1; j--) {
         bool sorted = true;
         size_t i;
@@ -28,7 +26,6 @@ void bubble_sort_in_place(int* array, size_t len) {
 
 void insertion_sort_in_place(int* array, size_t len) {
     if (len == 0) return;
-    assert(len > 0);
     int current_value_copy;
     size_t compare_index;
     for (size_t current_index = 1; current_index < len; current_index++) {
@@ -45,19 +42,18 @@ void insertion_sort_in_place(int* array, size_t len) {
 
 void quick_sort_in_place(int* array, size_t len) {
     if (len == 0) return;
-    assert(len > 0);
     int pivot_copy = array[len - 1];
     // partition array and calculate pivot_index
     size_t pivot_index = 0;
     for (size_t current_index = 0; current_index < len - 1; current_index++)
         if (array[current_index] <= pivot_copy) {
-            swap_array_members(array, pivot_index, current_index);
             pivot_index++;
+            swap_array_members(array, pivot_index - 1, current_index);
         }
     swap_array_members(array, pivot_index, len - 1);
     // recursive part
     quick_sort_in_place(array, pivot_index);
-    quick_sort_in_place(array + pivot_index, len - pivot_index - 1);
+    quick_sort_in_place(array + pivot_index + 1, len - pivot_index - 1);
 }
 
 int* mk_random_array(int *len) {
@@ -75,7 +71,16 @@ int* mk_random_array(int *len) {
     return array;
 }
 
-void demonstrate_sort_in_place(void (*f)(int*, size_t)) {
+bool is_sorted(int* array, size_t len) {
+    bool sorted = true;
+    for (size_t i = 1; i < len; i++)
+        if (array[i - 1] > array[i])
+            sorted = false;
+    return sorted;
+}
+
+void _demonstrate_sort_in_place(void (*f)(int*, size_t), char* name) {
+    printf("Demonstrating %s\n", name);
     int len;
     int* array = mk_random_array(&len);
     printintv(array, len);
@@ -83,15 +88,20 @@ void demonstrate_sort_in_place(void (*f)(int*, size_t)) {
     (*f)(array, len);
     printintv(array, len);
     printf("\n");
+    if (is_sorted(array, len))
+        printf("correctly sorted!\n");
+    else printf("incorrectly sorted.\n");
     free(array);
 }
+
+#define demonstrate_sort_in_place(name) _demonstrate_sort_in_place(&name, #name)
 
 int main(int argc, char** argv) {
     unsigned int seed;
     while (scanf("%u", &seed) <= 0);
     srand(seed);
-    demonstrate_sort_in_place(&bubble_sort_in_place);
-    demonstrate_sort_in_place(&insertion_sort_in_place);
-    demonstrate_sort_in_place(&quick_sort_in_place);
+    demonstrate_sort_in_place(bubble_sort_in_place);
+    demonstrate_sort_in_place(insertion_sort_in_place);
+    demonstrate_sort_in_place(quick_sort_in_place);
     return 0;
 }
