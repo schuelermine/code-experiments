a476f94acdfbff5a643d891f1f4239f480e5f419
Anselm Schüler <mail@anselmschueler.com>
2022-11-16 08:17:08 +0100

    black

--- Python/mk_decorator.py
+++ Python/mk_decorator.py
@@ -1,5 +1,7 @@
 from inspect import signature
 from functools import wraps
+
+
 def mk_decorator(*args):
     """
     Transform a plain function that takes multiple parameters into a decorator in one of its parameters.
@@ -16,26 +18,38 @@ def mk_decorator(*args):
     >
     > foo is None  # True
     """
+
     @wraps(mk_decorator)
     def _mk_decorator(dec, key):
         params = signature(dec).parameters
         if params == 1:
             return dec
         if isinstance(key, int):
+
             def modify_args(args, kwargs, fun):
                 args = list(args)
                 args.insert(key, fun)
                 return args, kwargs
+
             if key not in params:
-                raise ValueError(f"Argument to mk_decorator() does not take argument number {key}")
+                raise ValueError(
+                    f"Argument to mk_decorator() does not take argument number {key}"
+                )
         elif isinstance(key, str):
+
             def modify_args(args, kwargs, fun):
                 kwargs[key] = fun
                 return args, kwargs
+
             if key not in params:
-                raise ValueError(f"Argument to mk_decorator() does not take argument {key}")
+                raise ValueError(
+                    f"Argument to mk_decorator() does not take argument {key}"
+                )
         else:
-            raise TypeError("Argument to mk_decorator() must be an instance of int or str")
+            raise TypeError(
+                "Argument to mk_decorator() must be an instance of int or str"
+            )
+
         @wraps(dec)
         def new_dec_outer(*args, **kwargs):
             @wraps(dec)
@@ -47,15 +61,22 @@ def mk_decorator(*args):
                 except AttributeError:
                     pass
                 return new_fun
+
             return new_dec_inner
+
         return new_dec_outer
+
     if len(args) != 1:
-        raise TypeError(f"mk_decorator() takes exactly one argument ({len(args)} given)")
+        raise TypeError(
+            f"mk_decorator() takes exactly one argument ({len(args)} given)"
+        )
     x = args[0]
     if callable(x):
         return _mk_decorator(x, 0)
     else:
+
         @wraps(mk_decorator)
         def mk_decorator_(f):
             return _mk_decorator(f, x)
+
         return mk_decorator_
