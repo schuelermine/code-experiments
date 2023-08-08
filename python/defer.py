babd4bb5dc66e79974e1fb5f40d89d691c5d8c57
Anselm Schüler <mail@anselmschueler.com>
2023-08-08 22:14:29 +0200

    Revert "Remove redundant check for "run" in name"
    This reverts commit 3a58d4daeeae81e433ab40ffda8ca121614544d7.

--- python/defer.py
+++ python/defer.py
@@ -12,7 +12,7 @@ class DeferMut:
         self.ops.clear()
 
     def __getattribute__(self, name):
-        if name in ["obj", "ops"]:
+        if name in ["obj", "ops", "run"]:
             return object.__getattribute__(self, name)
 
         val = getattr(self.obj, name)
@@ -46,7 +46,7 @@ class DeferCon:
         return obj
 
     def __getattribute__(self, name: str):
-        if name in ["obj", "ops"]:
+        if name in ["obj", "ops", "run"]:
             return object.__getattribute__(self, name)
 
         val = getattr(self.obj, name)
